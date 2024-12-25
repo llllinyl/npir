f9cbb3c374e67eca720dfb344b16c1bdf18670b4
llllinyl <43959099+llllinyl@users.noreply.github.com>
2024-12-25 22:58:14 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=/home/lyl/Desktop/ntl-11.5.1/lib");
+    println!("cargo:rustc-link-search=native={your_path}/ntl-11.5.1/lib");
     println!("cargo:rustc-link-lib=ntl");
     println!("cargo:rustc-link-lib=gmp");
     println!("cargo:rustc-link-lib=stdc++");
-    println!("cargo:rustc-link-search=native=/home/lyl/Desktop/PIR-experiment/npir/src");
+    println!("cargo:rustc-link-search=native={your_path}/npir/src");
     println!("cargo:rustc-link-lib=invmod");
 }
