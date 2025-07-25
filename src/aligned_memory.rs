2a6f18b161c88a288de8b310264aa49f64bde2d8
llllinyl <43959099+llllinyl@users.noreply.github.com>
2025-07-25 16:08:50 +0800

    Update aligned_memory.rs

--- src/aligned_memory.rs
+++ src/aligned_memory.rs
@@ -6,17 +6,17 @@ use std::{
 };
 
 const ALIGN_SIMD: usize = 64; // enough to support AVX-512
-pub type AlignedMemory64 = AlignedMemory<ALIGN_SIMD>;
+pub type AlignedMemory64<T> = AlignedMemory<T, ALIGN_SIMD>;
 
-pub struct AlignedMemory<const ALIGN: usize> {
-    p: *mut u64,
-    sz_u64: usize,
+pub struct AlignedMemory<T, const ALIGN: usize> {
+    p: *mut T,
+    len: usize,
     layout: Layout,
 }
 
-impl<const ALIGN: usize> AlignedMemory<{ ALIGN }> {
-    pub fn new(sz_u64: usize) -> Self {
-        let sz_bytes = sz_u64 * size_of::<u64>();
+impl<T, const ALIGN: usize> AlignedMemory<T, ALIGN> {
+    pub fn new(len: usize) -> Self {
+        let sz_bytes = len * size_of::<T>();
         let layout = Layout::from_size_align(sz_bytes, ALIGN).unwrap();
 
         let ptr;
@@ -25,37 +25,37 @@ impl<const ALIGN: usize> AlignedMemory<{ ALIGN }> {
         }
 
         Self {
-            p: ptr as *mut u64,
-            sz_u64,
+            p: ptr as *mut T,
+            len,
             layout,
         }
     }
 
-    pub fn as_slice(&self) -> &[u64] {
-        unsafe { from_raw_parts(self.p, self.sz_u64) }
+    pub fn as_slice(&self) -> &[T] {
+        unsafe { from_raw_parts(self.p, self.len) }
     }
 
-    pub fn as_mut_slice(&mut self) -> &mut [u64] {
-        unsafe { from_raw_parts_mut(self.p, self.sz_u64) }
+    pub fn as_mut_slice(&mut self) -> &mut [T] {
+        unsafe { from_raw_parts_mut(self.p, self.len) }
     }
 
-    pub unsafe fn as_ptr(&self) -> *const u64 {
+    pub unsafe fn as_ptr(&self) -> *const T {
         self.p
     }
 
-    pub unsafe fn as_mut_ptr(&mut self) -> *mut u64 {
+    pub unsafe fn as_mut_ptr(&mut self) -> *mut T {
         self.p
     }
 
     pub fn len(&self) -> usize {
-        self.sz_u64
+        self.len
     }
 }
 
-unsafe impl<const ALIGN: usize> Send for AlignedMemory<{ ALIGN }> {}
-unsafe impl<const ALIGN: usize> Sync for AlignedMemory<{ ALIGN }> {}
+unsafe impl<T, const ALIGN: usize> Send for AlignedMemory<T, ALIGN> {}
+unsafe impl<T, const ALIGN: usize> Sync for AlignedMemory<T, ALIGN> {}
 
-impl<const ALIGN: usize> Drop for AlignedMemory<{ ALIGN }> {
+impl<T, const ALIGN: usize> Drop for AlignedMemory<T, ALIGN> {
     fn drop(&mut self) {
         unsafe {
             dealloc(self.p as *mut u8, self.layout);
@@ -63,24 +63,24 @@ impl<const ALIGN: usize> Drop for AlignedMemory<{ ALIGN }> {
     }
 }
 
-impl<const ALIGN: usize> Index<usize> for AlignedMemory<{ ALIGN }> {
-    type Output = u64;
+impl<T, const ALIGN: usize> Index<usize> for AlignedMemory<T, ALIGN> {
+    type Output = T;
 
     fn index(&self, index: usize) -> &Self::Output {
         &self.as_slice()[index]
     }
 }
 
-impl<const ALIGN: usize> IndexMut<usize> for AlignedMemory<{ ALIGN }> {
+impl<T, const ALIGN: usize> IndexMut<usize> for AlignedMemory<T, ALIGN> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.as_mut_slice()[index]
     }
 }
 
-impl<const ALIGN: usize> Clone for AlignedMemory<{ ALIGN }> {
+impl<T: Clone, const ALIGN: usize> Clone for AlignedMemory<T, ALIGN> {
     fn clone(&self) -> Self {
-        let mut out = Self::new(self.sz_u64);
-        out.as_mut_slice().copy_from_slice(self.as_slice());
+        let mut out = Self::new(self.len);
+        out.as_mut_slice().clone_from_slice(self.as_slice());
         out
     }
 }
